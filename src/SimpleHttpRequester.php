5098c18de446ccf6b0629599b42d3e952f7c6d48
N. Venkat Raj <venkat@codinggeek.com>
2022-12-15 13:04:55 +0530

    use wp_remote_get

--- src/SimpleHttpRequester.php
+++ src/SimpleHttpRequester.php
@@ -4,45 +4,35 @@ namespace HtaccessCapabilityTester;
 
 class SimpleHttpRequester implements HttpRequesterInterface
 {
-    /**
-     * Make a HTTP request to a URL.
-     *
-     * @param  string  $url  The URL to make the HTTP request to
-     *
-     * @return  HttpResponse  A HttpResponse object, which simply contains body, status code and response headers.
-     *                        In case the request itself fails, the status code is "0" and the body should contain
-     *                        error description (if available)
-     */
-    public function makeHttpRequest($url)
-    {
-        // PS: We suppress the E_WARNING level error generated on failure
-        $body = @file_get_contents($url);
-        if ($body === false) {
-            //$body = '';
-            return new HttpResponse('The following request failed: file_get_contents(' . $url . ')', '0', []);
-        }
+	/**
+	 * Make a HTTP request to a URL.
+	 *
+	 * @param  string  $url  The URL to make the HTTP request to
+	 *
+	 * @return  HttpResponse  A HttpResponse object, which simply contains body, status code and response headers.
+	 *                        In case the request itself fails, the status code is "0" and the body should contain
+	 *                        error description (if available)
+	 */
+	public function makeHttpRequest($url)
+	{
+		$response = wp_remote_get($url);
+		if (is_wp_error($response)) {
+			return new HttpResponse('The following request failed: ' . $url, '0', []);
+		}
 
-        // $http_response_header materializes out of thin air when file_get_contents() is called
+		$body = wp_remote_retrieve_body($response);
+		if (is_wp_error($body)) {
+			return new HttpResponse('The following request failed: ' . $url, '0', []);
+		}
 
-        // Get status code
-        $statusLine = $http_response_header[0];
-        preg_match('{HTTP\/\S*\s(\d{3})}', $statusLine, $match);
-        $statusCode = $match[1];
+		$statusCode = wp_remote_retrieve_response_code($response);
 
-        // Create headers map
-        $headersMap = [];
-        foreach ($http_response_header as $header) {
-            $pos = strpos($header, ':');
-            if ($pos > 0) {
-                $fieldName = strtolower(trim(substr($header, 0, $pos)));
-                $value = trim(substr($header, $pos + 1));
-                if (!isset($headersMap[$fieldName])) {
-                    $headersMap[$fieldName] = $value;
-                } else {
-                    $headersMap[$fieldName] .= ', ' . $value;
-                }
-            }
-        }
-        return new HttpResponse($body, $statusCode, $headersMap);
-    }
+		// Create headers map
+		$headers = wp_remote_retrieve_headers($response)->getAll();
+		if (is_wp_error($body)) {
+			return new HttpResponse('The following request failed: ' . $url, '0', []);
+		}
+
+		return new HttpResponse($body, $statusCode, $headers);
+	}
 }
